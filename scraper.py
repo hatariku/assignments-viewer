ecdf0cfe6d390ce9edf13bfa2066018b11298281
畑佐　陸斗 <241205143@ccmailg.meijo-u.ac.jp>
2025-10-05 16:02:41 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 JST = timezone(timedelta(hours=9))
 
